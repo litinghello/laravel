b3ea638ec4cee942f80876e54a366fd2cb99c82c
liting <2863515010@qq.com>
2018-09-25 16:39:18 +0800

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">信息提示</div>
+                <div class="card-header">订单状态</div>
 
                 <div class="card-body">
                     @if (session('status'))

92c0bcc5ed0a7b379856c18792e73ae9e65bac38
kxl <kxlsccd>
2018-09-26 13:59:48 +0800

    v

--- resources/views/penalty/pay_order.blade.php
+++ resources/views/penalty/pay_order.blade.php
@@ -53,7 +53,7 @@
     {
         WeixinJSBridge.invoke(
             'getBrandWCPayRequest',
-                '{{$config}}',
+            <?php echo $config; ?>,
             function(res){
                 WeixinJSBridge.log(res.err_msg);
                 alert(res.err_code+res.err_desc+res.err_msg);
@@ -80,39 +80,6 @@
     }
 
 </script>
-{{--<script type="text/javascript">
-    //获取共享地址
-    function editAddress()
-    {
-        WeixinJSBridge.invoke(
-            'editAddress',
-            <?php echo $editAddress; ?>,
-            function(res){
-                var value1 = res.proviceFirstStageName;
-                var value2 = res.addressCitySecondStageName;
-                var value3 = res.addressCountiesThirdStageName;
-                var value4 = res.addressDetailInfo;
-                var tel = res.telNumber;
-
-                alert(value1 + value2 + value3 + value4 + ":" + tel);
-            }
-        );
-    }
-
-    window.onload = function(){
-        if (typeof WeixinJSBridge == "undefined"){
-            if( document.addEventListener ){
-                document.addEventListener('WeixinJSBridgeReady', editAddress, false);
-            }else if (document.attachEvent){
-                document.attachEvent('WeixinJSBridgeReady', editAddress);
-                document.attachEvent('onWeixinJSBridgeReady', editAddress);
-            }
-        }else{
-            editAddress();
-        }
-    };
-
-</script>--}}
 
 
 

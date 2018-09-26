28cc5bf1959f9f5c6fd6d234c532066dfe747bb5
liting <2863515010@qq.com>
2018-09-26 13:58:57 +0800

    update

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,7 @@ use App\User;
 use App\WechatAccount;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller

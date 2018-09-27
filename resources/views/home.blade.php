4ec4e6d0efb1208578f17f02162a4b49ab088311
liting <2863515010@qq.com>
2018-09-27 17:50:45 +0800

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -46,11 +46,13 @@
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
     <script type="text/javascript" >
+
         $(document).ready(function() {
             $('#table_info').DataTable( {
                 "processing": true,
                 "serverSide": true,
-                "ajax": "/penalties/order_data",
+                // "ajax": "/penalties/order_data",
+                "ajax": "/laravel/penalties/order_data",
                 columns: [
                     // { data: 'id', name: 'id' },
                     { data: 'order_number', name: 'order_number' },
@@ -59,8 +61,27 @@
                     { data: 'order_status', name: 'order_status' },
                     { data: 'updated_at', name: 'updated_at' },
                     {data: 'action', name: 'action', orderable: false, searchable: false}
-                ]
+                ],
+                language: {
+                    lengthMenu: '<select class="form-control input-xsmall">' + '<option value="1">1</option>' + '<option value="10">10</option>' + '<option value="20">20</option>' + '<option value="30">30</option>' + '<option value="40">40</option>' + '<option value="50">50</option>' + '</select>条记录',//左上角的分页大小显示。
+                    search: '<span class="label label-success">搜索：</span>',//右上角的搜索文本，可以写html标签
+                    paginate: {//分页的样式内容。
+                        previous: "上一页",
+                        next: "下一页",
+                        first: "第一页",
+                        last: "最后"
+                    },
+                    zeroRecords: "没有内容",//table tbody内容为空时，tbody的内容。
+                    //下面三者构成了总体的左下角的内容。
+                    info: "总共_PAGES_ 页，显示第_START_ 到第 _END_ ，筛选之后得到 _TOTAL_ 条，初始_MAX_ 条 ",//左下角的信息显示，大写的词为关键字。
+                    infoEmpty: "0条记录",//筛选为空时左下角的显示。
+                    infoFiltered: ""//筛选之后的左下角筛选提示，
+                },
+                paging: true,
+                pagingType: "full_numbers",//分页样式的类型
             });
         });
+        $("#table_info_filter input[type=search]").css({ width: "auto" });
+
     </script>
 @show
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,11 +8,10 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'CardWorld') }}</title>
 
     <!-- Scripts -->
     {{--<script src="{{ asset('js/app.js') }}" defer></script>--}}
-    {{--<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>--}}
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">

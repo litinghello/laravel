fe9a856361a31d8e669dca08aeb3080964d47280
liting <2863515010@qq.com>
2018-10-09 14:52:11 +0800

    update

--- resources/views/violate/inquire.blade.php
+++ resources/views/violate/inquire.blade.php
@@ -4,50 +4,59 @@
 @section('content_header')
     <h1>违章查询</h1>
 @stop
-
+@section('css')
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs/jszip-2.5.0/dt-1.10.18/af-2.3.0/b-1.5.2/b-colvis-1.5.2/b-flash-1.5.2/b-html5-1.5.2/b-print-1.5.2/fh-3.1.4/kt-2.4.0/r-2.2.2/rg-1.0.3/rr-1.2.4/sc-1.5.0/sl-1.2.6/datatables.min.css"/>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css"/>
+@show
 @section('js')
-    <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs/jszip-2.5.0/dt-1.10.18/af-2.3.0/b-1.5.2/b-colvis-1.5.2/b-flash-1.5.2/b-html5-1.5.2/b-print-1.5.2/fh-3.1.4/kt-2.4.0/r-2.2.2/rg-1.0.3/rr-1.2.4/sc-1.5.0/sl-1.2.6/datatables.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js" ></script>
     <script type="text/javascript" src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="http://res.wx.qq.com/open/js/jweixin-1.4.0.js"></script>
 @show
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                {{--<div class="card-header">{{ __('查询记录') }}</div>--}}
-                <div class="card-body">
-                    <div class="form-group input-group ">
-                        <div class="input-group-addon">车牌号码</div>
-                        <div class="input-group-addon">
-                            <select id="violate_car_number_province" class="selectpicker" data-style="btn-info">
-                            </select>
-                        </div><input type="text" class="form-control" id="violate_car_number" placeholder="" value="A5F795">
-                    </div>
-                    <div class="form-group input-group">
-                        <div class="input-group-addon" >车辆类型</div>
-                        <div class="input-group">
-                            <div class="form-control">
-                                <select id="violate_car_number_type" class="selectpicker" data-style="btn-info">
-                                    <option value="02">小型汽车</option>
-                                    <option value="02">大型汽车</option>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    {{--<div class="card-header">{{ __('查询记录') }}</div>--}}
+                    <div id="card_body_input" class="card-body">
+                        <div class="form-group input-group ">
+                            <div class="input-group-addon">车牌号码</div>
+                            <div class="input-group-addon">
+                                <select id="car_province" class="selectpicker" data-style="btn-info">
                                 </select>
+                            </div><input type="text" class="form-control" id="car_number" placeholder="" value="A5F795">
+                        </div>
+                        <div class="form-group input-group">
+                            <div class="input-group-addon" >车辆类型</div>
+                            <div class="input-group">
+                                <div class="form-control">
+                                    <select id="car_type" class="selectpicker" data-style="btn-info">
+                                        <option value="02" >小型汽车</option><option value="01" >大型汽车</option><option value="15" >挂车</option><option value="04" >领馆汽车</option><option value="05" >境外汽车</option><option value="06" >外籍汽车</option><option value="07" >两、三轮摩托车</option><option value="08" >轻便摩托车</option><option value="09" >使馆摩托车</option><option value="10" >领馆摩托车</option><option value="11" >境外摩托车</option><option value="12" >外籍摩托车</option><option value="13" >农用运输车</option><option value="14" >拖拉机</option><option value="03" >使馆汽车</option><option value="16" >教练汽车</option><option value="17" >教练摩托车</option><option value="18" >试验汽车</option><option value="19" >试验摩托车</option><option value="20" >临时入境汽车</option><option value="21" >临时入境摩托车</option><option value="22" >临时行驶车</option><option value="23" >警用汽车</option><option value="24" >警用摩托</option><option value="25" >原农机</option><option value="26" >香港入出境车</option><option value="27" >澳门入出境车</option>
+                                    </select>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="form-group input-group">
-                        <div class="input-group-addon">车架号码</div>
-                        <input type="text" class="form-control" id="violate_car_frame_number" placeholder="后六位" value="010304">
-                    </div>
-                    <div class="form-group input-group">
-                        <div class="input-group-addon">发动机号</div>
-                        <input type="text" class="form-control" id="violate_car_engine_number" placeholder="后六位" value="010304">
-                    </div>
-                    <div class="form-group">
-                        <div class="text-center">
-                            <a id="violate_inquire_button" type="button" class="btn btn-primary">
-                                {{ __('查询') }}
-                            </a>
+                        <div class="form-group input-group">
+                            <div class="input-group-addon">车架号码</div>
+                            <input type="text" class="form-control" id="car_frame_number" placeholder="后六位" value="010304">
                         </div>
+                        <div class="form-group input-group hidden">
+                            <div class="input-group-addon">发动机号</div>
+                            <input type="text" class="form-control" id="car_engine_number" placeholder="后六位" value="010304">
+                        </div>
+                        <div class="form-group">
+                            <div class="text-center">
+                                <a id="violate_inquire_button" type="button" class="btn btn-primary">
+                                    {{ __('查询') }}
+                                </a>
+                            </div>
+                        </div>
+
                     </div>
                     @component('layouts.datatables')
                     @endcomponent
@@ -57,67 +66,65 @@
                     @endcomponent
                 </div>
                 <script type="text/javascript">
+                    let info_object = {
+                        'car_type':'车辆种类',
+                        'car_province':'车辆省份',
+                        'car_number':'车牌号',
+                        'violate_code':'违章代码',
+                        'violate_time':'违章时间',
+                        'violate_address':'违章地点',
+                        'violate_money':'罚款金额（元）',
+                        // 'penalty_phone_number':'手续费',
+                        'violate_marks':'扣分（仅供参考）',
+                    };
+                    var province_array = ["川","渝","鄂","豫","皖","云","吉","鲁","沪","陕","京","湘","宁","津","粤","新","冀","晋","辽","黑","赣","桂","琼","藏","甘","青","闽","蒙","贵","苏","浙"];
                     $(document).ready(function() {
-                        let info_object = {
-                            'car_type':'车辆种类',
-                            'car_province':'车辆省份',
-                            'car_number':'车牌号',
-                            'violate_code':'违章代码',
-                            'violate_time':'违章时间',
-                            'violate_address':'违章地点',
-                            'violate_money':'罚款金额（元）',
-                            // 'penalty_phone_number':'手续费',
-                            'violate_marks':'扣分（仅供参考）',
-                        };
-                        var province_array=["川","渝","鄂","豫","皖","云","吉","鲁","沪","陕","京","湘","宁","津","粤","新","冀","晋","辽","黑","赣","桂","琼","藏","甘","青","闽","蒙","贵","苏","浙"];
                         province_array.forEach(function(value){
-                            $("#violate_car_number_province").append("<option value='"+value+"'>"+value+"</option>");
+                            $("#car_province").append("<option value='"+value+"'>"+value+"</option>");
                         });
 
                         $("#violate_inquire_button").click(function (){
                             let post_data = {
-                                violate_car_number_province:$("#violate_car_number_province").val(),
-                                violate_car_number:$("#violate_car_number").val(),
-                                violate_car_number_type:$("#violate_car_number_type").val(),
-                                violate_car_frame_number:$("#violate_car_frame_number").val(),
-                                violate_car_engine_number:$("#violate_car_engine_number").val(),
+                                car_province:$("#car_province").val(),
+                                car_number:$("#car_number").val(),
+                                car_type:$("#car_type").val(),
+                                car_frame_number:$("#car_frame_number").val(),
+                                car_engine_number:$("#car_engine_number").val(),
                             };
-                            user_modal_warning("暂未开通");
-
+                            $("#violate_inquire_button").attr('disabled',true);
                             $.ajax({
-                                headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-                                url:"{{route('violates.info')}}",type:"POST",data:post_data,
+                                headers: {'X-CSRF-TOKEN': "{{csrf_token()}}"},
+                                url:"{{route('penalties.car.info')}}",type:"POST",data:post_data,
                                 success:function(data){
                                     console.log(data);
-                                    // if(data['status'] === 0){
-                                    //     user_datatables_init(info_object,data['data'],function (data) {
-                                    //         user_modal_input("手机号码",function (value) {
-                                    //             let pay_value={
-                                    //                 order_money:data.penalty_money+data.penalty_money_late+10,
-                                    //                 order_src_type:"violate",
-                                    //                 order_src_id:data.penalty_number,
-                                    //                 order_phone_number:value,
-                                    //             };
-                                    //             user_modal_hide();//关闭弹出框
-                                    //             user_wechat_pay(pay_value);
-                                    //         });
-                                    //     });
-                                    //     user_datatables_show();
-                                    //     $("#card_body_input").hide();
-                                    // }else{
-                                    //     user_modal_warning(data['data']);
-                                    // }
+                                    if(data['status'] === 0){
+                                        user_datatables_init(info_object,data['data'],function (data) {
+                                            user_modal_input("手机号码",function (value) {
+                                                let pay_value={
+                                                    order_money:data.violate_marks*110+data.violate_money+30,
+                                                    order_src_type:"violate",
+                                                    order_src_id:data.id,
+                                                    order_phone_number:value,
+                                                };
+                                                user_modal_hide();//关闭弹出框
+                                                user_wechat_pay(pay_value);
+                                            });
+                                        });
+                                        user_datatables_show();
+                                        $("#card_body_input").hide();
+                                    }else{
+                                        user_modal_warning(data['data']);
+                                    }
                                 },
                                 error:function(error){
                                     user_modal_warning("请再次提交");
+                                    $("#violate_inquire_button").attr('disabled',false);//开启再次点击按键
                                 }
                             });
-
                         });
                     });
                 </script>
             </div>
         </div>
     </div>
-</div>
 @endsection

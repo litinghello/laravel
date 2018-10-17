0f17c28ea21b6bffdd0a6a3aad3bd2100b3994d6
liting <2863515010@qq.com>
2018-10-17 18:06:33 +0800

    update

--- app/Http/Controllers/WeChatsController.php
+++ app/Http/Controllers/WeChatsController.php
@@ -207,10 +207,18 @@ class WeChatsController extends Controller
 //            'total_fee' => 1, //因为是以分为单位，所以订单里面的金额乘以100
 //            'spbill_create_ip' => '123.12.12.123', // 可选，如不传该参数，SDK 将会自动获取相应 IP 地址
 //            'notify_url' => 'https://pay.weixin.qq.com/wxpay/pay.action', // 支付结果通知网址，如果不设置则会使用配置里的默认地址
-            'trade_type' => 'JSAPI',
+            'trade_type' => 'JSAPI', // JSAPI，NATIVE，APP...
             'openid' =>  $user_id,//TODO: 用户openid
 //            'openid' =>  "oiGyj0im2uCtxHX3_oFct-BDyOuA",//TODO: 用户openid
         ]);
+//        $attributes = [
+//            'trade_type'=>Order::NATIVE,
+//            'body'=>"商品描述",
+//            'detail'=>"商品简介",
+//            'out_trade_no'=>'sdfdsfdsgdsadadsafdadsa',
+//            'total_fee'=>'1',
+//            'notify_url'=>'http://pay.codeisland.cn/wx/pay/notify'
+//        ];
         if ($result['return_code'] == 'SUCCESS' && $result['result_code'] == 'SUCCESS') {
 //            $config = $pay->jssdk->bridgeConfig($result['prepay_id'],false); //WeixinJSBridge支付 返回 json 字符串，如果想返回数组，传第二个参数 false
             $config = $pay->jssdk->sdkConfig($result['prepay_id']); //JSSDK支付 返回数组

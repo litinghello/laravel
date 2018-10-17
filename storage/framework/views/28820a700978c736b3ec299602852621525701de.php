bff7969025764b4ac5cfd8dfb022401399c3fe15
liting <2863515010@qq.com>
2018-10-17 21:52:31 +0800

    update

--- storage/framework/views/28820a700978c736b3ec299602852621525701de.php
+++ storage/framework/views/28820a700978c736b3ec299602852621525701de.php
@@ -4,83 +4,147 @@
 <?php $__env->startComponent('layouts.resources'); ?>
 <?php echo $__env->renderComponent(); ?>
 <?php $__env->startSection('content'); ?>
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                
-                <div class="card-body">
-                    <?php if(session('status')): ?>
-                        <div class="alert alert-success" role="alert">
-                            <?php echo e(session('status')); ?>
 
-                        </div>
-                    <?php endif; ?>
-                    <?php $__env->startComponent('layouts.datatables'); ?>
-                    <?php echo $__env->renderComponent(); ?>
-                    <?php $__env->startComponent('layouts.modal'); ?>
-                    <?php echo $__env->renderComponent(); ?>
-                    <?php $__env->startComponent('layouts.wechat'); ?>
-                    <?php echo $__env->renderComponent(); ?>
-                        <script type="text/javascript">
-                            let info_object = {
-                                'order_number':'订单号',
-                                'order_money':'订单金额',
-                                'order_phone_number':'联系电话',
-                                'order_status':'状态',
-                                'updated_at':'时间',
-                            };
-                            var order_status={
-                                'invalid':"无效",
-                                'unpaid':'未支付',
-                                'paid':'已支付',
-                                'processing':'正在处理',
-                                'completed':'处理完成',
-                            };
-                            $(document).ready(function() {
-                                $.ajax({
-                                    type:"POST",
-                                    headers: {'X-CSRF-TOKEN': "<?php echo e(csrf_token()); ?>"},
-                                    url:"<?php echo e(route('order.get')); ?>",
-                                    data:"",
-                                    success:function(data){
-                                        if(data['status'] === 0){
-                                            data['data'].forEach(function (value) {
-                                                value.order_status = order_status[value.order_status];
-                                            });
-                                            user_datatables_init(info_object,data['data'],function (data) {
-                                                let html = "<div>金额:"+data['order_money']+"元</div>"+
-                                                            "<div>电话:"+data['order_phone_number']+"</div>"+"<br>是否确认支付？";
-                                                user_modal_comfirm(html,function () {
-                                                    // user_modal_warning("订单处理");
-                                                    // console.log(data);
-                                                    let pay_value={
-                                                        order_money:parseInt(data.order_money),
-                                                        order_src_type:data.order_src_type,
-                                                        order_src_id:data.order_src_id,
-                                                        order_phone_number:data.order_phone_number,
-                                                    };
-                                                    // user_modal_hide();//关闭弹出框
-                                                    user_wechat_pay(pay_value);
-                                                });
-                                            });
-                                            user_datatables_show();
-                                        }else{
-                                            user_modal_warning(data['data']);
-                                        }
-                                    },
-                                    error:function(error){
-                                        user_modal_warning("请再次提交");
-                                    }
-                                });
-                            });
-                        </script>
-                </div>
-            </div>
+    <?php if(!count($list)): ?>
+        <p class="help-block text-center well">没 有 记 录 哦！</p>
+    <?php else: ?>
+        <div class="table-responsive table-bordered">
+
+            <table class="table table-striped table-hover">
+                <thead>
+                <tr>
+                <tr >
+                    <th>订单号</th>
+                    <th>订单金额</th>
+                    <th>联系电话</th>
+                    <th>状态</th>
+                    <th>时间</th>
+                </tr>
+                </thead>
+                <tbody>
+
+                <?php $__currentLoopData = $list; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as &$data): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    <tr onclick="dataClick(<?php echo e(json_encode($data)); ?>)">
+                        <td><?php echo e($data->order_number); ?></td>
+                        <td><?php echo e($data->order_money); ?></td>
+                        <td><?php echo e($data->order_phone_number); ?></td>
+                        <td><?php if($data->order_status=='paid'): ?>已支付<?php elseif($data->order_status=='unpaid'): ?>未支付<?php elseif($data->order_status=='invalid'): ?>无效<?php elseif($data->order_status=='processing'): ?>正在处理<?php elseif($data->order_status=='completed'): ?>处理完成<?php endif; ?></td>
+                        <td><?php echo e($data->updated_at); ?></td>
+                    </tr>
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                </tbody>
+
+            </table>
+
+            <?php if(isset($page)): ?><?php echo $page; ?><?php endif; ?>
         </div>
-    </div>
-</div>
+    <?php endif; ?>
+
 <?php $__env->stopSection(); ?>
+
+
+<?php $__env->startComponent('layouts.modal'); ?>
+<?php echo $__env->renderComponent(); ?>
+<?php $__env->startComponent('layouts.wechat'); ?>
+<?php echo $__env->renderComponent(); ?>
+<script>
+    function dataClick(data){
+
+        let html = "<div>金额:"+data['order_money']+"元</div>"+
+            "<div>电话:"+data['order_phone_number']+"</div>"+"<br>是否确认支付？";
+        user_modal_comfirm(html,function () {
+            // user_modal_warning("订单处理");
+             console.log(data);
+            let pay_value={
+                order_money:parseInt(data.order_money),
+                order_src_type:data.order_src_type,
+                order_src_id:data.order_src_id,
+                order_phone_number:data.order_phone_number,
+            };
+            // user_modal_hide();//关闭弹出框
+            user_wechat_pay(pay_value);
+        });
+
+    }
+</script>
+
+
+
+    
+        
+            
+                
+                
+                    
+                        
+                            
+                        
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                        
+                            
+                                
+                                
+                                
+                                
+                                
+                            
+                            
+                                
+                                
+                                
+                                
+                                
+                            
+                            
+                                
+                                    
+                                    
+                                    
+                                    
+                                    
+                                        
+                                            
+                                                
+                                            
+                                            
+                                                
+                                                            
+                                                
+                                                    
+                                                    
+                                                    
+                                                        
+                                                        
+                                                        
+                                                        
+                                                    
+                                                    
+                                                    
+                                                
+                                            
+                                            
+                                        
+                                            
+                                        
+                                    
+                                    
+                                        
+                                    
+                                
+                            
+                        
+                
+            
+        
+    
+
+
 
 
 
